8914df39988d1805a4a4d4ced921ef9f2a6aa31b
Ben <hexenon@users.noreply.github.com>
2020-02-24 14:15:13 -0700

    add router

--- src/app.js
+++ src/app.js
@@ -1,24 +1,8 @@
 // web server
 const app = require('./web-server');
-const usersController = require('./controllers/users.controller');
-const authMiddleware = require('./auth/auth.middleware');
-app.all('*', (req, res, next) => {
-  console.log(`Request ${req.method}\n`, req.params, req.query, req.body);
-  next();
-})
-app.get('/', (req, res) => {
-  res.send('OK');
-})
+const router = require('./router');
 
-// todo: other endpoints.
-
-app.get('/user/:id', authMiddleware, usersController.getUser);
-app.get('/users', usersController.getUsers);
-app.post('/user', authMiddleware, usersController.createUser);
-app.put('/user/:userId', usersController.updateUser);
-app.delete('/user/:userId', usersController.deleteUser);
-
-
+app.use('/api', router);
 
 app.listen(8080, () => {
   console.log('http server listening in http://localhost:8080/api')
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
 const usersController = require('../controllers/users.controller');
+const authMiddleware = require('../auth/auth.middleware');
 
-module.exports = (app) => {
-  // Health Check
+const router = express.Router();
 
+router.all('*', (req, res, next) => {
+  console.log(`Request ${req.method}\n`);
+  next();
+})
 
-}
+router.get('/', (req, res) => {
+  res.send('OK');
+})
+
+// Users endpoints
+router.get('/user/:id', authMiddleware, usersController.getUser);
+router.get('/users', usersController.getUsers);
+router.post('/user', authMiddleware, usersController.createUser);
+router.put('/user/:userId', usersController.updateUser);
+router.delete('/user/:userId', usersController.deleteUser);
+
+module.exports = router;
